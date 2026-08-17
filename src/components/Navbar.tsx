fc5ea0590b9d7a7bec2320cba1feecff5276815a
Nexus <keshavsharma.uppcl@gmail.com>
2026-08-17 17:57:02 +0530

    Correct closing tag in Navbar component
    Fix closing tag for a div element in Navbar component.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -50,8 +50,7 @@ export const Navbar: React.FC<NavbarProps> = ({
               );
             })}
           </div>
-        </div>
-      </div>
+        </di>
 
       {/* Main Navbar */}
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
